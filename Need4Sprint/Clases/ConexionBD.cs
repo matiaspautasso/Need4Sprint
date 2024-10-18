54c19698870a6904ca8da9f563b61991813b6a7e
Tomas Irrazabal <tomito99irra@gmail.com>
2024-10-18 15:57:24 -0300

    Creacion de funcion AgregarUsuario y EliminarUsuario. Se precisa revisar

--- Need4Sprint/Clases/ConexionBD.cs
+++ Need4Sprint/Clases/ConexionBD.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using
-
-
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
@@ -15,14 +12,18 @@ namespace Need4Sprint.Clases
     internal class ConexionBD
     {
         private readonly string cadenaDeConexion;
+        private readonly string usuario;
         //fixme
         /*
          * Aca se modifica los valores una vez implementados los cambios
+         * en especial usuarioConstructor
          */
-        public ConexionBD(string baseDatos, string contraseña = "")
+        public ConexionBD(string baseDatos, string contraseña = ""/*,string usuarioConstructor*/)
         {
             string servidor = "localhost";
-            string usuario = "root";
+
+            //usuario = usuarioConstructor;
+            usuario = "root";
 
             cadenaDeConexion = $"Server={servidor};Database={baseDatos};User ID={usuario};Password={contraseña};" +
                                "Pooling=true;SslMode=none;";
@@ -51,10 +52,13 @@ namespace Need4Sprint.Clases
                 try
                 {
                     conexion.Open();
-                    string query = $"SELECT COUNT(*) FROM Usuarios WHERE nombre = {usuario} AND contrasena = {contrasena} ";
+                    string query = $"SELECT COUNT(*) FROM Usuarios WHERE nombre = @usuario AND contrasena = @contrasena ";
 
                     using (MySqlCommand cmd = new MySqlCommand(query, conexion))
                     {
+                        cmd.Parameters.AddWithValue("@usuario",usuario);
+                        cmd.Parameters.AddWithValue("@contrasena", contrasena);
+
                         int resultado = Convert.ToInt32(cmd.ExecuteScalar());
                         return resultado > 0 && resultado < 2;
                     }
@@ -67,6 +71,71 @@ namespace Need4Sprint.Clases
                 }
             }
         }
+        public bool AgregarUsuario(string empresa,string nombre,string apellido, string email, string contrasena,string rolID,DateTime fecha)
+        {
+            using(MySqlConnection conexion = new MySqlConnection(cadenaDeConexion))
+            {
+                try
+                {
+                    conexion.Open();
+                    string query = @"INSERT INTO usuarios (empresa, nombre, apellido, email, contraseña, rol_id, fecha_registro) 
+                        VALUES (@empresa, @nombre, @apellido, @email, @contrasena, @rolID, @fecha);";
+
+                    using(MySqlCommand cmd = new MySqlCommand( query, conexion))
+                    {
+                        cmd.Parameters.AddWithValue("@empresa",empresa);
+                        cmd.Parameters.AddWithValue("@nombre", nombre);
+                        cmd.Parameters.AddWithValue("@apellido",apellido);
+                        cmd.Parameters.AddWithValue("@email", email);
+                        cmd.Parameters.AddWithValue("@contrasena",contrasena);
+                        cmd.Parameters.AddWithValue("@rolID",rolID);
+                        cmd.Parameters.AddWithValue("@fecha",fecha);
+
+                        int filasAfectadas = cmd.ExecuteNonQuery();
+
+                        return filasAfectadas > 0;
+                    }
+
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine($"Error al insertar usuario: {e.Message}");
+                    return false;
+                }
+            }
+        }
+
+        /*
+         * fixme
+         * Se debe modificar para que aquel usuario que tenga los permisos pueda modificar al resto
+         */
+        public bool EliminarUsuario(string nombre, string contrasena)
+        {
+            using (MySqlConnection conexion = new MySqlConnection(cadenaDeConexion))
+            {
+                try
+                {
+                    conexion.Open();
+                    string query = "DELETE FROM Usuarios WHERE nombre = @nombre AND contrasena = @contrasena;";
+                    using (MySqlCommand cmd = new MySqlCommand(query,conexion))
+                    {
+                        cmd.Parameters.AddWithValue("@nombre",nombre);
+                        cmd.Parameters.AddWithValue("@contrasena",contrasena);
+                        
+                        int filasAfectadas = cmd.ExecuteNonQuery();
+                        return filasAfectadas > 0;
+                    }
+                    
+                    
+                }
+                catch (MySqlException e)
+                {
+                    return false;
+                }
+            }
+        }
+
+        
 
     }
 
